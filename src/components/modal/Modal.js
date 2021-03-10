05086936c4a8f416f99aded20509536d188f6f6d
chaehun <youspend8@gmail.com>
2021-03-10 17:26:57 +0900

    fix : 상세 Modal Inner Wrapper 추가

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -31,23 +31,25 @@ const Modal = ({ title, children, onClose, isOpen }) => {
     <div className="modal_wrapper" ref={wrapper}>
       <div className="modal_background" ref={background} onClick={onClickBackground}></div>
       <div className="modal" ref={modal}>
-        <div className="modal_header">
-          <span className="modal_back_btn" onClick={onClickBack}>
-            <svg 
-              height="26px" 
-              id="Layer_1" 
-              style={{enableBackground: 'new 0 0 512 512'}}
-              version="1.1" 
-              viewBox="0 0 512 512" 
-              xmlns="http://www.w3.org/2000/svg">
-              <polygon points="352,128.4 319.7,96 160,256 160,256 160,256 319.7,416 352,383.6 224.7,256 "/>
-            </svg>
-          </span>
-          <h2 className="modal_title">{ title }</h2>
-          <div style={{width: '50px'}}></div>
-        </div>
-        <div className="modal_section">
-          { children }
+        <div className="modal_inner_wrapper">
+          <div className="modal_header">
+            <span className="modal_back_btn" onClick={onClickBack}>
+              <svg 
+                height="26px" 
+                id="Layer_1" 
+                style={{enableBackground: 'new 0 0 512 512'}}
+                version="1.1" 
+                viewBox="0 0 512 512" 
+                xmlns="http://www.w3.org/2000/svg">
+                <polygon points="352,128.4 319.7,96 160,256 160,256 160,256 319.7,416 352,383.6 224.7,256 "/>
+              </svg>
+            </span>
+            <h2 className="modal_title">{ title }</h2>
+            <div style={{width: '50px'}}></div>
+          </div>
+          <div className="modal_section">
+            { children }
+          </div>
         </div>
       </div>
     </div>
